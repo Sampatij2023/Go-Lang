0557ec15dee89e5f1e45b62464ad86b1af16a73d
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-05-25 18:03:54 +0530

    Add files via upload

--- session6.5.slices.go
+++ session6.5.slices.go
@@ -7,15 +7,20 @@ func main() {
 	fmt.Println("Enter the size of array")
 	fmt.Scanln(&n)
 	mySlice := make([]int, n)
+	fmt.Println(" Enter the array elements ")
 	for i := 0; i < len(mySlice); i++ {
-		fmt.Println(" Enter the array elements ")
+
 		fmt.Scanln(&mySlice[i])
 	}
 	var del int
 	fmt.Println("Enter the index which is to be deleted")
 	fmt.Scanln(&del)
 
-	mySlice = append(mySlice[:del], mySlice[(del+1):]...)
-	fmt.Println(" my slice after removing the element is :", mySlice)
+	updateArray(mySlice, del)
 
+}
+
+func updateArray(A []int, k int) {
+	A = append(A[:k], A[(k+1):]...)
+	fmt.Println(" my slice after removing the element is :", A)
 }
