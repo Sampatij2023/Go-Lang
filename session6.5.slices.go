3e310e57ab867f747ce1aaf3e309ea065fed79d9
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-05-27 15:21:20 +0530

    Update session6.5.slices.go

--- session6.5.slices.go
+++ session6.5.slices.go
@@ -1,44 +1,75 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var n int
+// 	fmt.Println("Enter the size of Slice")
+// 	fmt.Scanln(&n)
+// 	mySlice := make([]int, n)
+// 	fmt.Println(" Enter the Slice elements ")
+// 	for i := 0; i < len(mySlice); i++ {
+
+// 		fmt.Scanln(&mySlice[i])
+// 	}
+// 	var del int
+// 	fmt.Println("Enter the index which is to be deleted")
+// 	fmt.Scanln(&indexOfElement)
+// 	if(indexOfElement<=(n-1) && indexOfElement>=1){
+// 		newArray :=deleteElementInSlice(mySlice, indexOfElement)
+// 		fmt.Println("My array after deleting the index element")
+// 		fmt.Println(newArray)
+// 	}else if (indexOfElement == 0 || indexOfElement == n{
+// 		newArray :=deleteBoundaryElementInSlice(mySlice, indexOfElement)
+// 		fmt.Println("My array after deleting the index element")
+// 		fmt.Println(newArray)
+// 	}
+// 	esle{
+// 		fmt.Println("index is out of range of the array")
+// 	}
+
+// }
+
+// func deleteElementInSlice(A []int, k int)[]int {
+// 	A = append(A[:k], A[(k+1):]...)
+// 	return A;
+// }
+// func deleteBoundaryElementInSlice(A []int, k int)[]int {
+// 	if(k==0){	
+// 		return A[1:];
+// 	}
+// 	else{
+// 		return A[:k];
+// 	}
+// }
 package main
 
 import "fmt"
 
 func main() {
 	var n int
-	fmt.Println("Enter the size of Slice")
+	fmt.Println("Enter the size of array")
 	fmt.Scanln(&n)
 	mySlice := make([]int, n)
-	fmt.Println(" Enter the Slice elements ")
+	fmt.Println(" Enter the array elements ")
 	for i := 0; i < len(mySlice); i++ {
 
 		fmt.Scanln(&mySlice[i])
 	}
 	var del int
 	fmt.Println("Enter the index which is to be deleted")
-	fmt.Scanln(&indexOfElement)
-	if(indexOfElement<=(n-1) && indexOfElement>=1){
-		newArray :=deleteElementInSlice(mySlice, indexOfElement)
+	fmt.Scanln(&del)
+	if del <= n && del >= 0 {
+		newArray := deleteArray(mySlice, del)
 		fmt.Println("My array after deleting the index element")
 		fmt.Println(newArray)
-	}else if (indexOfElement == 0 || indexOfElement == n{
-		newArray :=deleteBoundaryElementInSlice(mySlice, indexOfElement)
-		fmt.Println("My array after deleting the index element")
-		fmt.Println(newArray)
-	}
-	esle{
+	} else {
 		fmt.Println("index is out of range of the array")
 	}
 
 }
 
-func deleteElementInSlice(A []int, k int)[]int {
+func deleteArray(A []int, k int) []int {
 	A = append(A[:k], A[(k+1):]...)
-	return A;
-}
-func deleteBoundaryElementInSlice(A []int, k int)[]int {
-	if(k==0){	
-		return A[1:];
-	}
-	else{
-		return A[:k];
-	}
+	return A
 }
