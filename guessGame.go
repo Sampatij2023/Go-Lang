30aea41146c2f0dec3cfab80d2e53dbf3d880309
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-06-03 15:41:24 +0530

    Add files via upload
    it have not tested it

---
+++ guessGame.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	randomNumber := rand.Intn(50)
+	for i := 0; i < 5; i++ {
+		fmt.Println("guess the number")
+		inputNumber, _ := fmt.Scanln()
+		if inputNumber > randomNumber {
+			fmt.Println("Your number is greater than the random number")
+		} else if inputNumber < randomNumber {
+			fmt.Println("Your number is smaller than the random number")
+		} else {
+			fmt.Println("great you have found the random number")
+			break
+		}
+	}
+}
