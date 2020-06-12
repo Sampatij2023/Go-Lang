e2ed2d5dd32ed3bb6ae8229622c2f015fe6edc6c
Sampatij2023 <58723466+Sampatij2023@users.noreply.github.com>
2020-06-12 16:21:11 +0530

    Update qn06.go

--- sol/qn06.go
+++ sol/qn06.go
@@ -34,25 +34,25 @@ type Address struct {
 func main() {
 	var subscriber Subscriber
 	subscriber.Name = "Aman Singh"
-	subscriber.AddressDetails.Street = "123 Oak St"
-	subscriber.AddressDetails.City = "Omaha"
-	subscriber.AddressDetails.State = "NE"
-	subscriber.AddressDetails.PostalCode = "68111"
+	subscriber.Address.Street = "123 Oak St"
+	subscriber.Address.City = "Omaha"
+	subscriber.Address.State = "NE"
+	subscriber.Addresss.PostalCode = "68111"
 	fmt.Println("Name:", subscriber.Name)                             // => Name: Aman Singh
-	fmt.Println("Street:", subscriber.AddressDetails.Street)          // => Street: 123 Oak St
-	fmt.Println("City:", subscriber.AddressDetails.City)              // => City: Omaha
-	fmt.Println("State:", subscriber.AddressDetails.State)            // => State: NE
-	fmt.Println("Postal Code:", subscriber.AddressDetails.PostalCode) // => Postal Code: 68111
+	fmt.Println("Street:", subscriber.Address.Street)          // => Street: 123 Oak St
+	fmt.Println("City:", subscriber.Address.City)              // => City: Omaha
+	fmt.Println("State:", subscriber.Address.State)            // => State: NE
+	fmt.Println("Postal Code:", subscriber.Address.PostalCode) // => Postal Code: 68111
 
 	var employee Employee
 	employee.Name = "Joy Carr"
-	employee.AddressDetails.Street = "456 Elm St"
-	employee.AddressDetails.City = "Portland"
-	employee.AddressDetails.State = "OR"
-	employee.AddressDetails.PostalCode = "97222"
+	employee.Address.Street = "456 Elm St"
+	employee.Address.City = "Portland"
+	employee.Address.State = "OR"
+	employee.Address.PostalCode = "97222"
 	fmt.Println("Name:", employee.Name)                             // => Name: Joy Carr
-	fmt.Println("Street:", employee.AddressDetails.Street)          // => Street: 456 Elm St
-	fmt.Println("City:", employee.AddressDetails.City)              // => City: Portland
-	fmt.Println("State:", employee.AddressDetails.State)            // => State: OR
-	fmt.Println("Postal Code:", employee.AddressDetails.PostalCode) // => Postal Code: 97222
+	fmt.Println("Street:", employee.Address.Street)          // => Street: 456 Elm St
+	fmt.Println("City:", employee.Address.City)              // => City: Portland
+	fmt.Println("State:", employee.Address.State)            // => State: OR
+	fmt.Println("Postal Code:", employee.Address.PostalCode) // => Postal Code: 97222
 }
